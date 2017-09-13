39cafede2f4427b0e4003792db60852c908e18b6
Jeppe Feld <jeppe.feld@capgemini.com>
2017-09-13 14:02:18 +0200

    Initial work on APIs

--- src/main/java/com/bankapp/controller/UserController.java
+++ src/main/java/com/bankapp/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.bankapp.controller;
 
-import javax.validation.Valid;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bankapp.model.User;
@@ -16,10 +15,47 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
+	private String name;
+	
 	// Create user
 	
 	@PostMapping("/users")
-	public User createUser(@Valid @RequestBody User user) {
-		return userRepository.save(user);
+	public void createUser(String name) {
+		
+		if (name == null)
+		{
+			System.out.println("error no name");
+		}
+		else
+		{
+			try
+			{
+				User user = new User(name, "test", "test", "test", new Date());
+				userRepository.save(user);
+			}
+			catch (Exception e)
+			{
+				System.out.println("error: "+e);
+			}
+		}
+		
+		
+
 	}
+	
+	@PostMapping("/testUsers")
+	public void test() {
+		System.out.println("testing");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println("saved name");
+		this.name = name;
+	}
+	
+	
 }
