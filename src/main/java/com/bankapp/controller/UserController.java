433ea3bfc5453cf52922d6eeb5549d92ca38d8b8
Kieu Nguyen <kieung@hotmail.com>
2017-09-13 15:25:56 +0200

    trying to route and update users to database

--- src/main/java/com/bankapp/controller/UserController.java
+++ src/main/java/com/bankapp/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.bankapp.controller;
 
-import java.util.Date;
-
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bankapp.model.User;
 import com.bankapp.repository.UserRepository;
@@ -15,47 +17,66 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
-	private String name;
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public ModelAndView showRegistration(ModelAndView modelAndView) {
+		
+		modelAndView.addObject("user", new User());
+		modelAndView.setViewName("register");
+		return modelAndView;
+				
+	}
+	
+	@RequestMapping(value="/access-denied", method=RequestMethod.POST)
+	public ModelAndView processRegistration(ModelAndView modelAndView, User user) {
+		userRepository.save(user);
+		modelAndView.setViewName("access-denied");
+		return modelAndView;
+			
+	}
+	
+	
+	
+//	private String name;
 	
 	// Create user
 	
-	@PostMapping("/users")
-	public void createUser(String name) {
-		
-		if (name == null)
-		{
-			System.out.println("error no name");
-		}
-		else
-		{
-			try
-			{
-				User user = new User(name, "test", "test", "test", new Date());
-				userRepository.save(user);
-			}
-			catch (Exception e)
-			{
-				System.out.println("error: "+e);
-			}
-		}
-		
-		
-
-	}
-	
-	@PostMapping("/testUsers")
-	public void test() {
-		System.out.println("testing");
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		System.out.println("saved name");
-		this.name = name;
-	}
+//	@PostMapping("/users")
+//	public void createUser(String name) {
+//		
+//		if (name == null)
+//		{
+//			System.out.println("error no name");
+//		}
+//		else
+//		{
+//			try
+//			{
+//				User user = new User(name, "test", "test", "test", new Date());
+//				userRepository.save(user);
+//			}
+//			catch (Exception e)
+//			{
+//				System.out.println("error: "+e);
+//			}
+//		}
+//		
+//		
+//
+//	}
+//	
+//	@PostMapping("/testUsers")
+//	public void test() {
+//		System.out.println("testing");
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		System.out.println("saved name");
+//		this.name = name;
+//	}
 	
 	
 }
--- src/main/java/com/bankapp/model/User.java
+++ src/main/java/com/bankapp/model/User.java
@@ -30,7 +30,7 @@ public class User {
 	}
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String name;
