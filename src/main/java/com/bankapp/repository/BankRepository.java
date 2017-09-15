3d6fccbd71794e1291f0bf1363891978f5ee01f2
Kieu Nguyen <kieung@hotmail.com>
2017-09-15 09:59:44 +0200

    lol

--- src/main/java/com/bankapp/repository/BankRepository.java
+++ src/main/java/com/bankapp/repository/BankRepository.java
@@ -1,7 +1,6 @@
 package com.bankapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.bankapp.model.BankAccount;
 
 public interface BankRepository extends JpaRepository<BankAccount, Long>{
